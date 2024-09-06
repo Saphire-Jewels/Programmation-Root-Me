4aeab04af83e751d78fcb7c559ea3b7242ee7fe5
IntiShadow <70452454+Saphire-Jewels@users.noreply.github.com>
2024-09-06 15:09:40 +0200

    tcp_encoder_decoder.py

--- tcp_encoder_decoder.py
+++ tcp_encoder_decoder.py
@@ -1,48 +1,29 @@
-import socket
-import re 
-import base64 
+import socket 
+import math
+import base64
 
-def decode(encoded_string):
-    # Décodage de la chaîne Base64
-    decoded_bytes = base64.b64decode(encoded_string)
-    decoded_string = decoded_bytes.decode('utf-8')
+host = "challenge01.root-me.org"
+port = 52023
 
-    # Affichage du résultat décodé
-    print(f"Decoded string: {decoded_string}")
-    return decoded_string
-
-# Créer un socket TCP 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Lier le socket à une adresse et un port
-server_address = ('challenge01.root-me.org', 52023)
-sock.connect(server_address)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    while True :
-        # Réception des données
-        data = sock.recv(1024).decode()
-        print(f"Raw data: {repr(data)}")
-
-
-        # Utilisation d'expression régulière pour trouver la chaîne de caractères encodée
-        match = re.search(r"my string is\s+'([^']+)'", data)
-
-        
-        if match:
-            encoded_string = match.group(1)  # Extraction de la chaîne encodée
-
-            # Effectuer le décodage
-            resultat = decode(encoded_string)
-            # Préparer la chaîne avec les caractères de terminaison "\r\n"
-            message = str(resultat) + "\r\n"
-
-            # Envoyer le message au serveur
-            sock.send(message.encode())
-            print(f"Envoyé: {resultat}")
-        else:
-            print("Aucune chaîne encodée trouvée dans les données reçues.")
-
+    client_socket.connect((host, port))
+    question = client_socket.recv(1024).decode()
+    print(question)
+    
+    search = question.split("'")
+    code = search[1]
+    
+    result = base64.b64decode(code).decode()
+    print(f"decoder = {result}")
+    
+    send_rep = (str(result) + "\n").encode()
+    
+    client_socket.send(send_rep)
+    
+    flag =  client_socket.recv(1024).decode()
+    
+    print(flag)
 finally:
-    # Fermer la connexion
-    sock.close()
-
+    client_socket.close()
